89252ee3c4772c1569e9fb5678c980807e385094
munhoven <munhoven@gmail.com>
2017-05-14 14:05:55 +0200

    Set up Main Screen Table View §4.35 (not compiled)

--- Freaking433/ViewController.swift
+++ Freaking433/ViewController.swift
@@ -8,18 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    // ^^^ added UITableViewDataSource and UITableViewDelegate ^^^
+    
+    @IBOutlet weak var mainTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        mainTableView.dataSource = self // how many rows
+        mainTableView.delegate = self // what goes into rows
+        
     }
-
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
+    
 }
 
